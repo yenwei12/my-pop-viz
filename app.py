5105332d214a37ee135b22ebbe3cad286d78b6dc
Yen Wei <yenwei24@gmail.com>
2023-06-25 20:52:59 +0800

    Add population map

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-
-DEBUG = False
+from population_map import populationMap
 
 # set page info
 st.set_page_config(
@@ -19,57 +18,42 @@ def setAllOptions(widget, options):
 st.title("Malaysia Population - Data Visualization")
 
 # --------------------------------------------------------------------------
-# widget on the same row
-left, right = st.columns(2, gap="large")
-# year range slider
-yearSliderLbl = "Year"
-selectedYear = left.slider(
-    label=yearSliderLbl,
-    min_value=1980,
-    max_value=2020,
-    key="year",
-    help="Select a year"
-)
-# gender multiselector
-genderMultiSelectorLbl = "Gender (Blank means Select All)"
-genders = sorted(["Male", "Female"])
-selectedGenders = right.multiselect(
-    label=genderMultiSelectorLbl,
-    options=genders,
-    key="genders"
-)
-selectedGenders = setAllOptions(selectedGenders, genders)
+# Filter options
+# --------------------------------------------------------------------------
+with st.sidebar:
+    # year range slider
+    yearSliderLbl = "Year"
+    selectedYear = st.slider(
+        label=yearSliderLbl,
+        min_value=1980,
+        max_value=2020,
+        key="year",
+        help="Select a year"
+    )
+
+    # state multiselector
+    stateMultiSelectorLbl = "State (Blank means Select All)"
+    states = sorted(["Johor", "Kedah", "Kelantan", "Melaka", "Negeri Sembilan",
+                    "Pulau Pinang", "Pahang", "Perak", "Perlis", "Sabah", "Sarawak",
+                     "Selangor", "Terengganu", "W.P. Kuala Lumpur", "W.P. Labuan",
+                     "W.P. Putrajaya"])
+    selectedStates = st.multiselect(
+        label=stateMultiSelectorLbl,
+        options=states,
+        key="states"
+    )
+    selectedStates = setAllOptions(selectedStates, states)
 # --------------------------------------------------------------------------
 
-# --------------------------------------------------------------------------
-# widget on the same row
-left, right = st.columns(2, gap="large")
-# ethnic multiselector
-ethnicMultiSelectorLbl = "Ethnic (Blank means Select All)"
-ethnics = sorted(["Bumiputera", "Malay", "Chinese",
-                 "Kadazan/Dusun", "Indians", "Bajau", "Murut", "Others"])
-selectedEthnics = left.multiselect(
-    label=ethnicMultiSelectorLbl,
-    options=ethnics,
-    key="ethnics"
-)
-selectedEthnics = setAllOptions(selectedEthnics, ethnics)
-# state multiselector
-stateMultiSelectorLbl = "State (Blank means Select All)"
-states = sorted(["Johor", "Kedah", "Kelantan", "Melaka", "Negeri Sembilan",
-                 "Pulau Pinang", "Pahang", "Perak", "Perlis", "Sabah", "Sarawak",
-                 "Selangor", "Terengganu", "W.P. Kuala Lumpur", "W.P. Labuan",
-                 "W.P. Putrajaya"])
-selectedStates = right.multiselect(
-    label=stateMultiSelectorLbl,
-    options=states,
-    key="states"
-)
-selectedStates = setAllOptions(selectedStates, states)
-# --------------------------------------------------------------------------
+# message to be sent to js
+props = {
+    "year": st.session_state.year,
+    "states": st.session_state.states,
+}
 
-if DEBUG:
-    st.write(f"{selectedYear} is selected")
-    st.write(f"{selectedEthnics} is selected")
-    st.write(f"{selectedGenders} is selected")
-    st.write(f"{selectedStates} is selected")
+# draw population state graph
+st.header("Population Distribution by State in a Country")
+st.markdown("**Click** on the legend to display the state with a specific population density. Click again to deselect.")
+st.markdown(
+    "**Hover** over the states to view detailed population distribution by ethnicity.")
+iPopulationMap = populationMap(key="popMap", **props)
---
+++ population_map/frontend/main.js
@@ -0,0 +1,247 @@
+// Global variables
+let states;
+let statePops;
+let totalPops;
+let colorScale;
+let selectedYear;
+let yearData;
+let highlightedColor = null;
+let tooltip;
+
+// Define the properties of the map container
+let width = 800;
+let height = 550;
+
+function highlightStates(color) {
+    d3.selectAll("path").style("opacity", 0.06);
+    d3.selectAll("path")
+        .filter(function () {
+            return d3.select(this).style("fill") === color;
+        })
+        .style("opacity", 1);
+}
+
+function unhighlightStates() {
+    d3.selectAll("path").style("opacity", 1).style("stroke", null);
+}
+
+function drawTooltip(state) {
+    if (!tooltip) {
+        tooltip = d3
+            .select("body")
+            .append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 1);
+    }
+
+    let stateData = statePops.filter(
+        (entry) =>
+            entry["State"] === state &&
+            entry["Year"] == selectedYear &&
+            entry["Sex"] === "Total Sex" &&
+            entry["Age Group"] === "Total Age Group" &&
+            entry["Ethnic group"] != "Total Ethnic Group"
+    );
+
+    let totalStatePopulation = stateData.reduce(
+        (acc, curr) => acc + Number(curr["Value ('000 Person)"]),
+        0
+    );
+    totalStatePopulation = Math.round(totalStatePopulation * 10) / 10;
+
+    let tooltipHtml;
+    if (stateData.length > 0) {
+        tooltipHtml = `<strong>${state}</strong><br>`;
+        tooltipHtml += `Total Population: ${totalStatePopulation} ('000 Person)<br>`;
+        stateData.forEach((entry) => {
+            let percentage = entry["Percentage"];
+            tooltipHtml += `${entry["Ethnic group"]}: ${percentage}%<br>`;
+        });
+    } else {
+        tooltipHtml = `${state}: No data in year ${selectedYear}`;
+    }
+
+    tooltip.transition().style("opacity", 0.9);
+    tooltip
+        .html(tooltipHtml)
+        .style("left", event.pageX + "px")
+        .style("top", event.pageY - 28 + "px");
+}
+
+function drawLegend() {
+    let legendContainerHeight = 200;
+
+    let legendContainer = d3
+        .select("#map-container")
+        .append("svg")
+        .attr("width", 60)
+        .attr("height", legendContainerHeight)
+        .style("position", "absolute")
+        .style("right", "0")
+        .style("bottom", "0")
+        .style("transform", "translateY(-50%)");
+
+    let colorSteps = colorScale.range().length;
+    let colorStepHeight = legendContainerHeight / colorSteps;
+
+    colorScale.range().forEach((color, i) => {
+        legendContainer
+            .append("rect")
+            .attr("width", 30)
+            .attr("height", colorStepHeight)
+            .attr("y", i * colorStepHeight)
+            .style("fill", color)
+            .style("cursor", "hand")
+            .on("click", () => {
+                // Check if the clicked color is already highlighted
+                if (highlightedColor === color) {
+                    unhighlightStates();
+                    highlightedColor = null;
+                } else {
+                    highlightStates(color);
+                    highlightedColor = color;
+                }
+            });
+    });
+
+    // Append labels to the legend container
+    legendContainer
+        .append("text")
+        .attr("x", 35)
+        .attr("y", 14)
+        .text("Low")
+        .attr("alignment-baseline", "middle")
+        .style("font-size", "12px")
+        .style("font-family", "sans-serif");
+
+    legendContainer
+        .append("text")
+        .attr("x", 35)
+        .attr("y", 189)
+        .text("High")
+        .attr("alignment-baseline", "middle")
+        .style("font-size", "12px")
+        .style("font-family", "sans-serif");
+}
+
+function drawPopulationText() {
+    let selectedPopulationData = totalPops.find(
+        (d) => +d["Year"] === selectedYear
+    );
+    d3.select("#population").text(
+        `Total population in ${selectedYear}: ${selectedPopulationData["TotalPopulation"]}`
+    );
+}
+
+function drawMap() {
+    let center = d3.geoCentroid(states);
+    let scale = 2800;
+    let translate = [width / 2 + 100, height / 2];
+    let geoProjection = d3
+        .geoMercator()
+        .center(center)
+        .scale(scale)
+        .translate(translate);
+    let geoGenerator = d3.geoPath().projection(geoProjection);
+
+    let mapContainer = d3
+        .select("#map-container")
+        .selectAll("svg.map")
+        .data([states])
+        .join("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
+
+    mapContainer
+        .selectAll("path")
+        .data(states.features)
+        .join("path")
+        .attr("d", geoGenerator)
+        .style("cursor", "pointer")
+        .attr("fill", function (d) {
+            let matchedData = yearData.find(
+                (entry) => entry["State"] === d.properties.name
+            );
+            return matchedData
+                ? colorScale(matchedData["Value ('000 Person)"])
+                : "#ccc";
+        })
+        .attr("stroke", "#000")
+        .attr("stroke-width", "1.5")
+        .on("mouseover", function (event, d) {
+            // Skip unhighlight states
+            if (
+                highlightedColor &&
+                d3.select(this).style("fill") !== highlightedColor
+            ) {
+                return;
+            }
+
+            d3.select(this).attr("fill", "YELLOW");
+            drawTooltip(d.properties.name);
+        })
+        .on("mouseout", function (event, d) {
+            tooltip.transition().duration(500).style("opacity", 0);
+            d3.select(this).attr("fill", function () {
+                let matchedData = yearData.find(
+                    (entry) => entry["State"] === d.properties.name
+                );
+                if (matchedData) {
+                    return colorScale(matchedData["Value ('000 Person)"]);
+                } else {
+                    return "#ccc";
+                }
+            });
+        });
+}
+
+function update(year) {
+    selectedYear = parseInt(year);
+
+    // Retrieve the population value for each state
+    let populationData = statePops.filter(
+        (d) =>
+            d["Ethnic group"] === "Total Ethnic Group" &&
+            d["Sex"] === "Total Sex" &&
+            d["Age Group"] === "Total Age Group"
+    );
+
+    yearData = populationData.filter((d) => +d["Year"] === selectedYear);
+
+    // Define a color scale for mapping population values to colors
+    colorScale = d3
+        .scaleQuantize()
+        .domain([0, d3.max(yearData, (d) => +d["Value ('000 Person)"])])
+        .range(d3.quantize((t) => d3.interpolateGreens(t * 0.8 + 0.1), 7));
+
+    drawLegend();
+    drawMap();
+    drawPopulationText();
+    drawDeathChart("Johor", 2005);
+}
+
+// Access values sent from Streamlit
+function onRender(event) {
+    console.log(event.data.args);
+    let year = event.data.args.year;
+
+    // load topojson
+    d3.json("data/my-states.json").then((data) => {
+        states = topojson.feature(data, data.objects.states);
+    });
+
+    // load data
+    d3.csv("data/pop-total.csv").then((data) => {
+        totalPops = data;
+    });
+
+    d3.csv("data/pop-states.csv").then((data) => {
+        statePops = data;
+        update(year);
+    });
+}
+
+Streamlit.events.addEventListener(Streamlit.MSG_RENDER, onRender);
+Streamlit.setComponentReady();
+Streamlit.setFrameHeight(height * 10);
